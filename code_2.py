741788d9b08cb3daf221b5b3315e79dcd9e5dde1
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2023-11-20 18:36:53 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,27 @@
+    return data
+def generate_random_data():
+    for item in data:
+    data = generate_random_data()
+
+import random
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    main()
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():def generate_random_data():
+
+    for item in data:
+import random
+
+
+if __name__ == "__main__":
+    return data
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
